e724400c46d2978c45f39fa8131e28d8c2682416
francisuloko <francisuloko@yahoo.com>
2022-05-23 23:18:45 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import Clock from './components/Clock';
+import "./App.css";
+import Clock from "./components/Clock";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout />
       <Clock />
     </div>
   );
