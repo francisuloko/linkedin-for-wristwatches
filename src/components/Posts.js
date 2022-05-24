9ba9f57a67fa0cf1e274cff0fb2aab4dca3ee5e3
francisuloko <francisuloko@yahoo.com>
2022-05-24 05:12:58 +0100

    Modify post component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,14 +2,15 @@ import React from "react";
 
 export default function Posts() {
   const posts = [
-    { author: "Francis Uloko", headline: "Top 10 devs to follow on LinkedIn" },
-    { author: "Francis Uloko", headline: "Top 10 devs to follow on LinkedIn" },
-    { author: "Francis Uloko", headline: "Top 10 devs to follow on LinkedIn" },
+    { author: "Nedu Omega", headline: "Top 10 devs to follow on LinkedIn" },
+    { author: "Muria Lopez", headline: "5 ways to waste your every day" },
+    { author: "Company Page", headline: "Top 10 devs to follow on LinkedIn" },
   ];
+
   const postsList = posts.map((post) => {
     <div className="post">
-      <p>{post.author}</p>
-      <span> {post.headline}</span>
+      <span className="author">{post.author}</span>
+      <span className="headline">{post.headline}</span>
     </div>;
   });
 
