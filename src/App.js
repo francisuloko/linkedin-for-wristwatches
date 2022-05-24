078d4f4a3010f26bcb011da3f0612ce44e9cd13c
francisuloko <francisuloko@yahoo.com>
2022-05-24 02:19:12 +0100

    Add timer to clockface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Nav from "./components/nav";
+import Clock from "./components/Clock"
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app vh-100 d-flex flex-column gap-2 justify-content-center align-items-center">
+      <div className="clock-face d-flex flex-column justify-content-around align-items-center">
+      <spa className="logo">LinkedIn <i class="bi bi-linkedin text-primary"></i></spa>
       <Nav />
+      <Clock />
+    </div>
     </div>
   );
 }
