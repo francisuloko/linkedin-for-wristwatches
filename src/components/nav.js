a88d35fe3497e8d2a503766d0cdc2797bb26248d
francisuloko <francisuloko@yahoo.com>
2022-05-24 03:18:16 +0100

    Add alerts counter

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,12 +5,32 @@ export default function Nav() {
   return (
     <div className="nav w-100">
       <Carousel interval={null} indicators={false} className="w-100 ">
-        <Carousel.Item><i className="bi bi-house-fill"></i></Carousel.Item>
-        <Carousel.Item><i className="bi bi-people-fill"></i></Carousel.Item>
-        <Carousel.Item><i className="bi bi-briefcase-fill"></i></Carousel.Item>
-        <Carousel.Item><i className="bi bi-chat-dots-fill"></i></Carousel.Item>
-        <Carousel.Item><i className="bi bi-bell-fill"></i></Carousel.Item>
-    </Carousel>
+        <Carousel.Item>
+          <i className="bi bi-house-fill position-relative p-2 position-relative p-2">
+            <span className="position-absolute bg-danger">99+</span>
+          </i>
+        </Carousel.Item>
+        <Carousel.Item>
+          <i className="bi bi-people-fill position-relative p-2">
+            <span className="position-absolute bg-danger">99+</span>
+          </i>
+        </Carousel.Item>
+        <Carousel.Item>
+          <i className="bi bi-briefcase-fill position-relative p-2">
+            <span className="position-absolute bg-danger">99+</span>
+          </i>
+        </Carousel.Item>
+        <Carousel.Item>
+          <i className="bi bi-chat-dots-fill position-relative p-2">
+            <span className="position-absolute bg-danger">99+</span>
+          </i>
+        </Carousel.Item>
+        <Carousel.Item>
+          <i className="bi bi-bell-fill position-relative p-2">
+            <span className="position-absolute bg-danger">99+</span>
+          </i>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
