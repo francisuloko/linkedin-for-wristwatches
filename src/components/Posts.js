5f1718d2c1024a98b293df796e4695a7e031fac1
francisuloko <francisuloko@yahoo.com>
2022-05-24 20:37:44 +0100

    Router posts and pass state

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Posts() {
   const posts = [
-    { author: "Nedu Omega", headline: "Top 10 devs to follow on LinkedIn" },
-    { author: "Muria Lopez", headline: "5 ways to waste your every day" },
-    { author: "Company Page", headline: "Top 10 devs to follow on LinkedIn" },
-    { author: "Nedu Omega", headline: "Top 10 devs to follow on LinkedIn" },
-    { author: "Muria Lopez", headline: "5 ways to waste your every day" },
-    { author: "Company Page", headline: "Top 10 devs to follow on LinkedIn" },
+    {
+      id: 1,
+      author: "Nedu Omega",
+      headline: "Top 10 devs to follow on LinkedIn",
+    },
+    {
+      id: 2,
+      author: "Muria Lopez",
+      headline: "5 ways to waste your every day",
+    },
+    {
+      id: 3,
+      author: "Company Page",
+      headline: "Top 10 devs to follow on LinkedIn",
+    },
+    {
+      id: 4,
+      author: "React Group",
+      headline: "Hooked: Everything React Hooks",
+    },
+    { id: 5, author: "Tolla Mirror", headline: "May the 4th be with you" },
+    { id: 6, author: "Hydrogen Man", headline: "Nich, Nich, Template" },
   ];
 
   const postsList = posts.map((post, i) => (
-    <div key={i} className="post d-flex flex-column m-1 py-1 px-2 rounded">
+    <Link
+      to={"/posts/" + post.id}
+      state={{ post }}
+      key={post.id}
+      className="post d-flex flex-column m-1 py-1 px-2 rounded"
+    >
       <span className="author">{post.author}</span>
       <span className="headline">{post.headline}</span>
-    </div>
+    </Link>
   ));
 
   return <div className="w-100">{postsList}</div>;
