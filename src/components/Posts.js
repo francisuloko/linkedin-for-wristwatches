d919f1ff9612424f3f8c4cc9b6f05af1b8549a03
francisuloko <francisuloko@yahoo.com>
2022-05-24 16:49:05 +0100

    Add classes to post and more data

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,14 +5,17 @@ export default function Posts() {
     { author: "Nedu Omega", headline: "Top 10 devs to follow on LinkedIn" },
     { author: "Muria Lopez", headline: "5 ways to waste your every day" },
     { author: "Company Page", headline: "Top 10 devs to follow on LinkedIn" },
+    { author: "Nedu Omega", headline: "Top 10 devs to follow on LinkedIn" },
+    { author: "Muria Lopez", headline: "5 ways to waste your every day" },
+    { author: "Company Page", headline: "Top 10 devs to follow on LinkedIn" },
   ];
 
-  const postsList = posts.map((post) => {
-    <div className="post">
+  const postsList = posts.map((post, i) => (
+    <div key={i} className="post d-flex flex-column m-1 py-1 px-2 rounded">
       <span className="author">{post.author}</span>
       <span className="headline">{post.headline}</span>
-    </div>;
-  });
+    </div>
+  ));
 
-  return <div>{postsList}</div>;
+  return <div className="w-100">{postsList}</div>;
 }
