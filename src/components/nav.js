35a176ff3aa910e8d257ce1cb295997fe4daf304
francisuloko <francisuloko@yahoo.com>
2022-05-24 14:30:39 +0100

    Modify alerts counter

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import Carousel from "react-bootstrap/Carousel";
 export default function Nav() {
   return (
     <div className="nav w-100">
-      <Carousel interval={null} indicators={false} controls={false} className="w-100 ">
+      <Carousel interval={null} indicators={false} controls={true} className="w-100 ">
         <Carousel.Item>
           <i className="bi bi-house-fill position-relative p-2 position-relative p-2">
             <span className="position-absolute bg-danger">99+</span>
@@ -12,22 +12,22 @@ export default function Nav() {
         </Carousel.Item>
         <Carousel.Item>
           <i className="bi bi-people-fill position-relative p-2">
-            <span className="position-absolute bg-danger">99+</span>
+            <span className="position-absolute bg-danger">3</span>
           </i>
         </Carousel.Item>
         <Carousel.Item>
           <i className="bi bi-briefcase-fill position-relative p-2">
-            <span className="position-absolute bg-danger">99+</span>
+            <span className="position-absolute bg-danger">5</span>
           </i>
         </Carousel.Item>
         <Carousel.Item>
           <i className="bi bi-chat-dots-fill position-relative p-2">
-            <span className="position-absolute bg-danger">99+</span>
+            <span className="position-absolute bg-danger">10</span>
           </i>
         </Carousel.Item>
         <Carousel.Item>
           <i className="bi bi-bell-fill position-relative p-2">
-            <span className="position-absolute bg-danger">99+</span>
+            <span className="position-absolute bg-danger">12</span>
           </i>
         </Carousel.Item>
       </Carousel>
