ef92bf9b5a27a4b523fd78a5ac5ff1486ad180eb
francisuloko <francisuloko@yahoo.com>
2022-05-24 16:48:27 +0100

    Add links to navigation

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,34 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 
 export default function Nav() {
   return (
-    <div className="nav w-100">
-      <Carousel interval={null} indicators={false} controls={true} className="w-100 ">
+    <div className="nav w-100 d-flex align-items-center justify-content-center">
+      <Carousel
+        interval={null}
+        indicators={false}
+        controls={true}
+        className="w-100"
+      >
         <Carousel.Item>
-          <i className="bi bi-house-fill position-relative p-2 position-relative p-2">
-            <span className="position-absolute bg-danger">99+</span>
-          </i>
+          <Link to="/posts">
+            <i className="bi bi-house-fill position-relative p-2 position-relative p-2">
+              <span className="position-absolute bg-danger">99+</span>
+            </i>
+          </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <i className="bi bi-people-fill position-relative p-2">
-            <span className="position-absolute bg-danger">3</span>
-          </i>
+          <Link to="/jobs">
+            <i className="bi bi-people-fill position-relative p-2">
+              <span className="position-absolute bg-danger">3</span>
+            </i>
+          </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <i className="bi bi-briefcase-fill position-relative p-2">
-            <span className="position-absolute bg-danger">5</span>
-          </i>
+          <Link to="/jobs">
+            <i className="bi bi-briefcase-fill position-relative p-2">
+              <span className="position-absolute bg-danger">5</span>
+            </i>
+          </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <i className="bi bi-chat-dots-fill position-relative p-2">
-            <span className="position-absolute bg-danger">10</span>
-          </i>
+          <Link to="/messages">
+            <i className="bi bi-chat-dots-fill position-relative p-2">
+              <span className="position-absolute bg-danger">10</span>
+            </i>
+          </Link>
         </Carousel.Item>
         <Carousel.Item>
-          <i className="bi bi-bell-fill position-relative p-2">
-            <span className="position-absolute bg-danger">12</span>
-          </i>
+          <Link to="/notifications">
+            <i className="bi bi-bell-fill position-relative p-2">
+              <span className="position-absolute bg-danger">12</span>
+            </i>
+          </Link>
         </Carousel.Item>
       </Carousel>
     </div>
