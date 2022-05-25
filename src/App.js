68dc994b3209024be3914444168ff4cfcafaa2d2
francisuloko <francisuloko@yahoo.com>
2022-05-26 00:40:18 +0100

    Add clock design

--- src/App.js
+++ src/App.js
@@ -14,33 +14,51 @@ function App() {
   const screenName = window.location.pathname;
 
   return (
-    <div className="app vh-100 d-flex flex-column gap-2 justify-content-center align-items-center">
-      <div className="clock-face d-flex flex-column justify-content-around align-items-center position-relative">
-        <Link to="/" className="logo mt-3">
-          LinkedIn <i className="bi bi-linkedin text-primary"></i>
-        </Link>
-        <div id="main">
-          <Routes>
-            <Route path="/" exact element={<Nav />} />
-            <Route path="/feed" exact element={<Feed />} />
-            <Route path="/feed/:id" exact element={<Post />} />
-            <Route path="/inbox" exact element={<Inbox />} />
-            <Route path="/inbox/:id" exact element={<Message />} />
-            <Route path="/network" exact element={<Network />} />
-            <Route path="/notification" exact element={<Notifications />} />
-          </Routes>
+    <div class="watch-container">
+      <div class="watch-strap-top"></div>
+
+      <div class="watch">
+        <div class="watch-face">
+          <div class="watch-face-outline">
+            <div className="app">
+              <div className="clock-face d-flex flex-column justify-content-around align-items-center position-relative">
+                <Link to="/" className="logo mt-3">
+                  LinkedIn <i className="bi bi-linkedin text-primary"></i>
+                </Link>
+                <div id="main">
+                  <Routes>
+                    <Route path="/" exact element={<Nav />} />
+                    <Route path="/feed" exact element={<Feed />} />
+                    <Route path="/feed/:id" exact element={<Post />} />
+                    <Route path="/inbox" exact element={<Inbox />} />
+                    <Route path="/inbox/:id" exact element={<Message />} />
+                    <Route path="/network" exact element={<Network />} />
+                    <Route
+                      path="/notification"
+                      exact
+                      element={<Notifications />}
+                    />
+                  </Routes>
+                </div>
+                {screenName === "/" ? (
+                  <Clock />
+                ) : (
+                  <button
+                    className="back bg-transparent fs-5 border-0 m-0 p-0"
+                    onClick={() => navigate(-1)}
+                  >
+                    <i className="bi bi-chevron-left"></i>
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        {screenName === "/" ? (
-          <Clock />
-        ) : (
-          <button
-            className="back bg-transparent fs-5 border-0 m-0 p-0"
-            onClick={() => navigate(-1)}
-          >
-            <i className="bi bi-chevron-left"></i>
-          </button>
-        )}
+
+        <div class="watch-crown"></div>
       </div>
+
+      <div class="watch-strap-bottom"></div>
     </div>
   );
 }
