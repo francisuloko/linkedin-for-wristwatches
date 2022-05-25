a30a12eec25a6438439fdf792614c1696d1503d2
francisuloko <francisuloko@yahoo.com>
2022-05-25 22:50:35 +0100

    Add like property to posts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,32 +2,46 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Feed() {
-  const Feed = [
+  const feed = [
     {
       id: 1,
       author: "Nedu Omega",
       headline: "Top 10 devs to follow on LinkedIn",
+      like: 3,
     },
     {
       id: 2,
       author: "Muria Lopez",
       headline: "5 ways to waste your every day",
+      like: 35,
     },
     {
       id: 3,
       author: "Company Page",
       headline: "Top 10 devs to follow on LinkedIn",
+      like: 634,
     },
     {
       id: 4,
       author: "React Group",
       headline: "Hooked: Everything React Hooks",
+      like: 31,
     },
-    { id: 5, author: "Tolla Mirror", headline: "May the 4th be with you" },
-    { id: 6, author: "Hydrogen Man", headline: "Nich, Nich, Template" },
+    {
+      id: 5,
+      author: "Tolla Mirror",
+      headline: "May the 4th be with you",
+      like: 3,
+    },
+    {
+      id: 6,
+      author: "Hydrogen Man",
+      headline: "Nich, Nich, Template",
+      like: 0,
+    },
   ];
 
-  const posts = Feed.map((post, i) => (
+  const posts = feed.map((post, i) => (
     <Link
       to={"/Feed/" + post.id}
       state={{ post }}
@@ -42,8 +56,10 @@ export default function Feed() {
     </Link>
   ));
 
-  return <div className="w-100">
-    <span className="px-1">Feed</span>
-    {posts}
-  </div>;
+  return (
+    <div className="w-100">
+      <span className="px-1">Feed</span>
+      {posts}
+    </div>
+  );
 }
